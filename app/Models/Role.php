154f5c7dc9da9dade95d8d2c79576f21ed4e794d
Jooker302 <i.omer6637@gmail.com>
2023-08-06 15:35:24 +0500

    Roles added

---
+++ app/Models/Role.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $table = 'roles';
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
